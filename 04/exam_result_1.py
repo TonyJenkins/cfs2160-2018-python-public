818bceabd9056a9e49f81fb31133a7d1a835ac67
Tony Jenkins <tony.jenkins@elder-studios.co.uk>
2018-10-10 17:17:25 +0100

    First cut at week 5 programs.

---
+++ 04/exam_result_1.py
@@ -0,0 +1,21 @@
+name = input ('Enter the student\'s name: ')
+
+mark_1 = int (input ('Enter first result:  '))
+mark_2 = int (input ('Enter second result: '))
+mark_3 = int (input ('Enter third result:  '))
+mark_4 = int (input ('Enter fourth result: '))
+mark_5 = int (input ('Enter fifth result:  '))
+
+total_marks = mark_1 + mark_2 + mark_3 + mark_4 + mark_5
+
+average_mark = total_marks / 5
+
+print ()
+print ('Final Mark for ' + name + ' is ' + str (average_mark) + '.')
+
+if average_mark >= 70.0:
+    print ('Cool Beans! ' + name + ' has a Distinction!')
+elif average_mark >= 50.0:
+    print ('Congratulations! ' + name + ' has passed!')
+else:
+    print ('Sadly, ' + name + ' has failed. What a pity.')
