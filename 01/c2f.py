5c23fd9c3a6611fc7dd6c0f05f850021474ceabd
Tony Jenkins <A,Jenkins@hud.ac.uk>
2018-10-11 16:55:45 +0100

    Add sample circle area and celsius to fahrenheit programs.

---
+++ 01/c2f.py
@@ -0,0 +1,7 @@
+"""
+    Convert Celsius to Fahrenheit.
+"""
+
+celsius = float (input ('Enter the temp in Celsius: '))
+
+print ('In Fahrenheit that is', celsius * 9.0/5.0 + 32)
---
+++ 01/circle.py
@@ -0,0 +1,9 @@
+"""
+    Find the area of a circle.
+"""
+
+from math import pi
+
+radius = float (input ('Enter the radius of the circle: '))
+
+print ('The area is', pi * radius * radius)
