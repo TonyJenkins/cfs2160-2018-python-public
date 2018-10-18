2c171d7f6d0f22843c1a7b568f6da75c4e9f3da5
Tony Jenkins <A.Jenkins@hud.ac.uk>
2018-10-18 15:18:37 +0100

    Move circle area and celsius to fahrenheit programs.

--- 01/c2f.py
+++
@@ -1,7 +0,0 @@
-"""
-    Convert Celsius to Fahrenheit.
-"""
-
-celsius = float (input ('Enter the temp in Celsius: '))
-
-print ('In Fahrenheit that is', celsius * 9.0/5.0 + 32)
---
+++ 02/c2f.py
@@ -0,0 +1,7 @@
+"""
+    Convert Celsius to Fahrenheit.
+"""
+
+celsius = int (input ('Enter the temp in Celsius: '))
+
+print ('In Fahrenheit that is', celsius * 9/5 + 32)
