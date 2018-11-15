e87d34c496e282cebfc4a91aa8a2b592347c5d51
Tony Jenkins <tony.jenkins@elder-studios.co.uk>
2018-11-15 15:20:19 +0000

    Add (flakey) email validation program from lecture. Caveat Emptor.

---
+++ 06/check_email.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+"""
+   Check if an email address looks valid.
+"""
+
+__author__    = "Tony Jenkins"
+__email__     = "tony.jenkins@elder-studios.co.uk"
+__date__      = "2018-11-15"
+
+def valid_email (email):
+
+    if email.count ('@') != 1:
+        return False
+
+    if email [0] == '@':
+        return False
+
+    if email.rfind ('.') < email.find ('@'):
+        return False
+
+    return True
+
+if __name__ == '__main__':
+    print (valid_email ("fred@pop.ac.uk"))
